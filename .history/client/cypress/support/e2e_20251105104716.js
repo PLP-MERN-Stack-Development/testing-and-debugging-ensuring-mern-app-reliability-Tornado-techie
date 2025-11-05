173a4b4f53541bc83d2c074b01efeb9fc3861a56
Salome Mundia <salomemundia13@gmail.com>
2025-11-05 22:08:36 +0300

    Remove .history folder and prevent future tracking
    - Add .history/ to .gitignore to exclude IDE history files
    - Remove all .history files from repository tracking
    - Clean up repository for professional submission

--- .history/client/cypress/support/e2e_20251105104716.js
+++
@@ -1,8 +0,0 @@
-// Cypress support file
-import './commands';
-
-// Global before each hook
-beforeEach(() => {
-  // Clear any existing data or set up test state
-  cy.log('Setting up test environment');
-});
